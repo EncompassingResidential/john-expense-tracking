a7f45b0f441e7a50d592a0ff27aae7e24f9542db
encompassingresidential <encompassing.residential@gmail.com>
2022-02-17 13:26:59 -0500

    renamed style.css to React src directory.  Starting to refactor Scrimba project.

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './Header.js'
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,12 +12,6 @@ import img_path from "../src/logo192moved.png";
 const img_width = "100px";
 
 // All React coding goes in the src directory.
-/*
-From Scrimba React
-  <MyAwesomeNavBar  />
-   <MainContent />
-
-*/
 
 function MainContent() {
   return (
@@ -24,35 +20,30 @@ function MainContent() {
      <h3>React function names are Case SenSITIVE.  When I change a React function name's first character from Upper Case to lower case the function disappears, but does not create a compile error.</h3>
      React calls this Composable.
      <h3>React is Declarative.  Vanilla Javascript is Imperative</h3>
+     <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Waike</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+
      </div>
   )
 }
 
-const element = (
-  <h1 className="header">This is JSX 2/15/22 4:03 pm {Date.now()}</h1>
-)
-console.log(JSON.stringify(element))
 
-function TemporaryName() {
+function App() {
   return (
-  <div>
-    <img src={img_path} width={img_width} />
-    <h6>img_path</h6>
-    <h1>Fun facts about React</h1>
-    <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Waike</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-
-  </div>
+    <>
+      <Header />
+      <MainContent />
+    </>
   )
 }
 
 ReactDOM.render(
-  <TemporaryName />,
+  <App />,
 
   /* This doesn't work
   TemporaryName()
@@ -60,17 +51,3 @@ ReactDOM.render(
    */
   document.getElementById("rootExpenseTrackerApp")
 );
-
-
-function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <h1>Bob's Bistro</h1>
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">Menu</li>
-            <li className="nav-item">About</li>
-            <li className="nav-item">Contact</li>
-          </ul>
-        </nav>
-    )
-}
---
+++ expense-tracker/src/Header.js
@@ -0,0 +1,23 @@
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <h1>Bob's Bistro</h1>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">Menu</li>
+            <li className="nav-item">About</li>
+            <li className="nav-item">Contact</li>
+          </ul>
+        </nav>
+    )
+}
+
+export default function Header() {
+    return (
+    <div>
+      <img src={img_path} width={img_width} />
+  
+    </div>
+    )
+  }
+  
+  
