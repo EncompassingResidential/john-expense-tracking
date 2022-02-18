13f515ef724bf307a103a0233278ab804dd5f6c1
encompassingresidential <encompassing.residential@gmail.com>
2022-02-17 21:17:08 -0500

    Finished Scrimba Project 1 - big stuff with CSS and figured out how to import CSS file in React.

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -3,42 +3,32 @@ import ReactDOM from 'react-dom';
 
 import Header from './Header.js'
 
-import logo from './logo.svg';
 import './App.css';
-
-// Works "../src/logo192moved.png"
-import img_path from "../src/logo192moved.png";
-
-const img_width = "100px";
 
 // All React coding goes in the src directory.
 
 function MainContent() {
   return (
-    <div>
-     <h1>MainContent Here</h1>
-     <h3>React function names are Case SenSITIVE.  When I change a React function name's first character from Upper Case to lower case the function disappears, but does not create a compile error.</h3>
-     React calls this Composable.
-     <h3>React is Declarative.  Vanilla Javascript is Imperative</h3>
-     <ul>
+    <main>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
           <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Waike</li>
+          <li>Was originally created by Jordan Walke</li>
           <li>Has well over 100K stars on GitHub</li>
           <li>Is maintained by Facebook</li>
           <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-
-     </div>
+    </main>
   )
 }
 
 
 function App() {
   return (
-    <>
+    <div class="header-main">
       <Header />
       <MainContent />
-    </>
+    </div>
   )
 }
 
--- expense-tracker/src/Header.js
+++ expense-tracker/src/Header.js
@@ -1,12 +1,14 @@
+import logo from './logo.svg';
+
+// Works "../src/logo192moved.png"
+import img_path from "../src/logo192moved.png";
+
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <h1>Bob's Bistro</h1>
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">Menu</li>
-            <li className="nav-item">About</li>
-            <li className="nav-item">Contact</li>
-          </ul>
+            <img src={img_path} className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
 }
@@ -14,8 +16,7 @@ function Navbar() {
 export default function Header() {
     return (
     <div>
-      <img src={img_path} width={img_width} />
-  
+       <Navbar />
     </div>
     )
   }
