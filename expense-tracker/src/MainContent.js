57f12702fcacf27f590ea4c69a4ee72f61a37944
encompassingresidential <encompassing.residential@gmail.com>
2022-02-19 19:47:40 -0800

    Finished AirBnB Scrimba project 2.0

--- expense-tracker/src/MainContent.js
+++ expense-tracker/src/MainContent.js
@@ -7,62 +7,50 @@ import online_experience_3_Image_path from "./images/Mountain_Biking.png";
 
 import star_image from "./images/star.png";
 
-function Experience_Item_1(item_image) {
+function Experience_Item(props) {
   return (
     <div className="card">
-      <img src={online_experience_1_Image_path} className="card--image" />
+      <img src={props.image} className="card--image" />
       <div className="card--stats">
         <img src={star_image} className="star--image" />
-        <span>5.0</span>
-        <span>(6) • </span>
-        <span>USA</span>
+        <span>{props.score} </span>
+        <span className="gray">({props.number}) • </span>
+        <span className="gray">{props.country}</span>
       </div>
-      <p>Text for Experience Item</p>
-      <p>From $136 / person</p>
+      <p>{props.Experience_Description}</p>
+      <p><span className="bold">From ${props.money_amount}</span> / person</p>
     </div>
   )
 }
-
-function Experience_Item_2(item_image) {
-  return (
-    <div className="card">
-      <img src={online_experience_2_Image_path} className="card--image" />
-      <div className="card--stats">
-        <img src={star_image} className="star--image" />
-        <span>5.0</span>
-        <span>(30) • </span>
-        <span>USA</span>
-      </div >
-      <p>Learn wedding photography</p>
-      <p>From $money_amount / person</p>
-    </div>
-  )
-}
-
-function Experience_Item_3(item_image) {
-  return (
-    <div className="card">
-      <img src={online_experience_3_Image_path} className="card--image" />
-      <div className="card--stats">
-        <img src={star_image} className="star--image" />
-        <span>4.8</span>
-        <span>(2) • </span>
-        <span>USA</span>
-      </div >
-      <p>Group Mountain Biking</p>
-      <p>From $money_amount / person</p>
-    </div>
-  )
-}
-
 
 export default function Card() {
     return (
       <main>
         <div className="experience--grid">
-          <Experience_Item_1 />
-          <Experience_Item_2 />
-          <Experience_Item_3 />
+          <Experience_Item
+            image={online_experience_1_Image_path}
+            score="5.0"
+            number="4"
+            country="USA"
+            Experience_Description="Life Lessons with Katie Zaferes"
+            money_amount="136"
+          />
+          <Experience_Item 
+            image={online_experience_2_Image_path}
+            score="5.0"
+            number="30"
+            country="USA"
+            Experience_Description="Learn wedding photography"
+            money_amount="125"
+          />
+          <Experience_Item 
+            image={online_experience_3_Image_path}
+            score="4.8"
+            number="2"
+            country="USA"
+            Experience_Description="Group Mountain Biking"
+            money_amount="50"
+          />
         </div>
       </main>
     )
