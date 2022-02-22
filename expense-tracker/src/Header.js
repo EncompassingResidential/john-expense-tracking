bf1af43d8665ef8c96fb7346d137b0fad2f600e2
encompassingresidential <encompassing.residential@gmail.com>
2022-02-22 14:32:59 -0800

    Figuring out CSS inheritence of flex boxes and fonts.

--- expense-tracker/src/Header.js
+++ expense-tracker/src/Header.js
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <img src={earth_image} alt="Travel the Earth" className="nav--logo" />
-            <p>This should be here</p>
+            <p>My Travel Diary</p>
         </nav>
     )
 }
--- expense-tracker/src/MainContent.js
+++ expense-tracker/src/MainContent.js
@@ -6,21 +6,6 @@ import data from "./data.js";
 // import earth_image from "./images/earth_image.png";
 
 // 😱 img src Works after moving src/images folder to public/images
-/*
-function DiaryCard(props) {
-  // for pass in of item={item} you can create new Object let props = prop.item
-  // for pass in of {...item} don't need reassign to new Object.
-
-  // let ImageURL = ""
-  let ImageURL = (props.imageUrl.search("http") >= 0) ? ImageURL = props.imageUrl : ImageURL = "./images/" + props.imageUrl
-
-  return (
-    <div className="card">
-      <div>ImageURL !== "" && <div className="card--badge">ImageURL</div>
-    </div>
-  )
-}
-*/
 
 function DiaryCard(props) {
   console.log(props)
@@ -30,7 +15,7 @@ function DiaryCard(props) {
 
   return (
     <>
-      <div>Tell me more 11:23 am</div>
+      <div>Tell me more 2:32 pm 11:23 am</div>
       <div className="diary--entry">
         <p>Before Image {props.title}</p>
         <img src={imageToPrint} alt="This Description" className="diary--image" />
