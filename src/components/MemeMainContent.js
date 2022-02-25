9eff5bf94d3c8213664c9d99717a8a79616635ce
encompassingresidential <encompassing.residential@gmail.com>
2022-02-25 12:49:41 -0800

    Scrimba Section 3 finished input buttons and CSSing.  Started handling events.  Added memesData.

--- src/components/MemeMainContent.js
+++ src/components/MemeMainContent.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
+import memesData from './memesData.js'
+
 export default function MemeMainContent() {
+    function handleClick() {
+        console.log("handleClick @ " + Date.now())
+    }
+
+    function handleMouseOver() {
+        console.log("handleMouseOver @ " + Date.now())
+    }
+
+    function handleFocus() {
+        console.log("handleFocus @ " + Date.now())
+    }
+
     return (
-        <div>
-            <p>Hi MemeMainContent</p>
-        </div>
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button
+                    className="form--button"
+                    onClick={handleClick}
+                    onMouseOver={handleMouseOver}
+                    onFocus={handleFocus}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+        </main>
     )
 }
