14c38dbf4c23e29326b9796fcd935345b00839b8
encompassingresidential <encompassing.residential@gmail.com>
2022-02-27 12:06:24 -0500

    Scrimba learning State Change with React.useState

--- src/components/MemeMainContent.js
+++ src/components/MemeMainContent.js
@@ -3,20 +3,51 @@ import React from 'react';
 import memesData from './memesData.js'
 
 export default function MemeMainContent() {
-    function handleClick() {
-        console.log("handleClick @ " + Date.now())
-    }
 
-    function handleMouseOver() {
-        console.log("handleMouseOver @ " + Date.now())
-    }
+    let url
+    const [isImportant, setIsImportant] = React.useState("No")
 
-    function handleFocus() {
-        console.log("handleFocus @ " + Date.now())
-    }
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
 
+        url = memesArray[randomNumber].url
+
+        // This isImportant shows the previous value
+        console.log("\nB4 isImportant Previous Value " + isImportant)
+
+        /*  It changes the HTML to the current status of isImportant
+            THEN it runs this setIsImportant() code.
+            setIsImportant(function(oldValue) {
+                let newString = ""
+                if (oldValue === "Yes") 
+                newString = "No"
+                else {
+                    newString = "Yes"
+                }
+                return ( newString );
+            })
+            */
+           
+        // Both of these setIsImportant work
+        setIsImportant(oldValue => {
+           let newString = ""
+           if (oldValue === "Yes") 
+                newString = "No"
+            else {
+                newString = "Yes"
+            }
+            return ( newString );
+        })
+
+        console.log("11:45 C5 isImportant Previous Value " + isImportant)
+
+        console.log(url)
+    }
+    
     return (
         <main>
+            <p>{url} </p>
             <div className="form">
                 <input 
                     type="text"
@@ -30,11 +61,9 @@ export default function MemeMainContent() {
                 />
                 <button
                     className="form--button"
-                    onClick={handleClick}
-                    onMouseOver={handleMouseOver}
-                    onFocus={handleFocus}
+                    onClick={getMemeImage}
                 >
-                    Get a new meme image 🖼
+                    Get a new meme image 🖼 11:59 am isImportant starts as +NoN Now is {isImportant}
                 </button>
             </div>
         </main>
