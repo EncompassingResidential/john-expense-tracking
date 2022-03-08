d0a3790e4fbdeb746a71b8716721cb8ade4b4a92
encompassingresidential <encompassing.residential@gmail.com>
2022-03-08 12:54:12 -0500

    Updated README file, added App screenshot for Readme and Lennart reminded me where to change the App's webpage title string in public/index.html

--- src/components/ExpensesHeader.js
+++ src/components/ExpensesHeader.js
@@ -4,7 +4,7 @@ import earth_image from "../images/earth_image.png";
 export default function ExpensesHeader() {
     return (
     <div className="expense--header">
-            <img className="header--image" src={earth_image}  />
+            <img className="header--image" src={earth_image} alt="" />
             <h2 className="header--title">Expense Tracker App</h2>
             <h4 className="header--project">SDMM Project 4 Version 3/02/22</h4>
     </div>
