7103099647cc96273bb2655d06bd0b4475f20673
encompassingresidential <encompassing.residential@gmail.com>
2022-03-08 10:50:27 -0500

    Couldn't figure out how to make small image with React Bootstrap.  Left Header like it was pre-Bootstrap.

--- src/components/ExpensesHeader.js
+++ src/components/ExpensesHeader.js
@@ -1,18 +1,12 @@
-import { Card } from 'react-bootstrap'
 
 import earth_image from "../images/earth_image.png";
 
 export default function ExpensesHeader() {
     return (
-    <Card >
-        <header className="header">
-            <img 
-            src={earth_image} 
-            className="header--image"
-            />
+    <div className="expense--header">
+            <img className="header--image" src={earth_image}  />
             <h2 className="header--title">Expense Tracker App</h2>
             <h4 className="header--project">SDMM Project 4 Version 3/02/22</h4>
-        </header>
-    </Card>
+    </div>
     )
   }
